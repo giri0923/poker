e00be2f78884662a65a7ef235b53702593b1f645
Girish <girish.dasarathy@gmail.com>
2017-12-11 11:52:30 -0500

    changed test case

--- src/test/PokerTest.java
+++ src/test/PokerTest.java
@@ -1,5 +1,8 @@
 package test;
 
+import static org.junit.Assert.*;
+
+import org.junit.Assert;
 import org.junit.Test;
 
 import pokerFiveHand.CardType;
@@ -8,13 +11,14 @@ import pokerFiveHand.Poker;
 import pokerFiveHand.PokerCard;
 import pokerFiveHand.PokerHand;
 
+/**
+ * @author girish
+ *
+ */
 public class PokerTest {
-// Error - 4 different types
-	//number below 0 and above 12
-	@Test
-	public void test() {
-//PokerCard card = new PokerCard(1, CardType.Club);
 
+	@Test
+	public void test1() {
 		// one pair
 		PokerHand hand1 = new PokerHand(new PokerCard(CardValue.Ten, CardType.Diamond),
 				new PokerCard(CardValue.Ten, CardType.Club),
@@ -29,21 +33,8 @@ public class PokerTest {
 				new PokerCard(CardValue.Three, CardType.Diamond),
 				new PokerCard(CardValue.King, CardType.Diamond));
 
-		//0,9,10,11,12
-		//PokerHand hand2 = new PokerHand();
-
 		Poker poker = new Poker();
-		//hand1.getValueOfHand();
-		//System.out.println(hand1.getValueOfHand());
-		int winner = poker.compare(hand1, hand2);
-		if(winner==0)
-			System.out.println("Draw");
-		else
-
-		System.out.println("Winner is player" + winner);
-
-		//assertEquals();
-
+		assertEquals(2, poker.compare(hand1, hand2));
 	}
 
 }
